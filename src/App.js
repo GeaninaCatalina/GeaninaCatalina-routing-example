27994ff3769c32df52a92b9a331d8d23201e8e16
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-07-08 19:58:54 +0200

    -m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Suspense, Component }from 'react';
 import './App.css';
-import Dan from './Dan';
-import Everyone from './Everyone';
 import Geanina from './Geanina';
-import User from './User';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Button } from 'semantic-ui-react';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
 
@@ -15,14 +10,9 @@ class App extends Component {
       <div className="App">
           <h1 className="App-header">Hello</h1>
           <Router>
-          <Link to='/geanina'> to Geanina</Link>
-          <Button><Link to='/geanina'> to Geanina</Link></Button>
             <Suspense fallback={<div>Loading...</div>}>
               <Switch>
-                <Route exact path="/" component={Everyone}/>
-                <Route exact path="/dan" component={Dan}/>
                 <Route exact path="/geanina" component={Geanina}/>
-                <Route exact path="/user/:username" component={User} />
               </Switch>
             </Suspense>
           </Router>
--- src/Geanina.js
+++ src/Geanina.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
-function Geanina() {
-    console.log('rendering..')
-  return (
-    <div>
-      <h2>Geanina</h2>
-    </div>
-  );
+const heroes = [
+  {name: 'Batman', franchise: 'DC'},
+  {name: 'Ironman', franchise: 'Marvel'},
+  {name: 'Thor', franchise: 'Marvel'},
+  {name: 'Superman', franchise: 'DC'}
+];
+
+const people = [
+  {
+    name: 'James',
+    age: 31,
+  },
+  {
+    name: 'John',
+    age: 45,
+  },
+  {
+    name: 'Paul',
+    age: 65,
+  },
+  {
+    name: 'Ringo',
+    age: 49,
+  },
+  {
+    name: 'George',
+    age: 34,
+  }
+];
+
+class Geanina extends Component{
+  
+
+onClickFilter(){
+  console.log(heroes.filter(function(hero){
+    return hero.franchise ==='Marvel'
+  }))
+}
+
+onClickPersons(){
+  console.log(people.filter(individ => individ.age<65))
+}
+
+
+ render(){
+   return (
+     <div>
+       <Button onClick={this.onClickFilter}>Filter Marvel</Button>
+       <Button onClick={this.onClickPersons}>Filter Persons</Button>
+      <div>
+      {people.filter(individ => individ.age<65).map(filteredperson => 
+        (<li>{filteredperson.name}</li>))}
+      </div>
+
+     </div>
+   )
+ }
 }
 
 export default Geanina;
